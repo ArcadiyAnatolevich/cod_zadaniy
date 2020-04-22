603e3ace1508ad5e71764338c11a57ee515405aa
ArcadiyAnatolevich <timatiun@gmail.com>
2020-04-22 14:37:49 +0300

    1

--- src/task1.kt
+++ src/task1.kt
@@ -1,3 +1,22 @@
+import kotlin.math.PI
+
 fun main() {
-    println("hello")
+    val d: Double = readLine()?.toDouble() ?: 0.0
+    val r: Double = d / 2.0
+    val s: Double = sphe(r)
+    val v: Double = sph2(r)
+    println("площадь полной поверхности = $s")
+    println("объём шара = $v")
 }
+
+fun sphe(r: Double): Double {
+    val s: Double = 4.0 * PI * r * r
+    return s
+}
+
+fun sph2(r: Double): Double {
+    val v: Double = (4.0 * PI * r * r * r) / 3.0
+    return v
+}
+
+
