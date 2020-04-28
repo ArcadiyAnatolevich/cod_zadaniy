3eb1cc81bf8812dc1a93b412f54e60944a5b85bb
ArcadiyAnatolevich <timatiun@gmail.com>
2020-04-28 16:13:39 +0300

    1

--- src/task3/task3.kt
+++ src/task3/task3.kt
@@ -1,11 +1,18 @@
-
 fun main() {
     val a: Int = readLine()?.toInt() ?: 0
-    if (a in 2..32) {
-        println("между 2 и 32")
-    } else if (a > 32) {
-        println("больше 32")
-    } else { (a < 2)
+
+    fun norm() {
+        if (a in 2..32)
+            println("между 2 и 32")
+    }
+
+    fun right() {
+        else if (a > 32)
+            println("больше 32")
+    }
+
+    fun left() {
+        else (a < 2)
         println("меньше 2")
     }
 }
