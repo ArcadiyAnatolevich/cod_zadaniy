ff976a3f205aad5adde3865f6ca4a60a7dc5d239
ArcadiyAnatolevich <timatiun@gmail.com>
2020-09-27 14:43:59 +0300

    1

--- src/task10.kt
+++ src/task10.kt
@@ -1,9 +1,16 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    val o: Int = readLine()?.toInt() ?: 0
+    var o: Int = readLine()?.toInt() ?: 0
     val a = -16
     val b = 16
-    while (a < b) {
-        o > 16
-        o < -16
+    val j = 1
+    ///////////////////////
+    while (o > j) {
+        o > a
+        o < b
+        o -+ j
+        println(o)
     }
+    /////////////////////////
 }
