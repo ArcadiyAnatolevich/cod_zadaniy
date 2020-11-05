a3a5b2919303f78c000739b514ed3bbd6fd21c42
ArcadiyAnatolevich <timatiun@gmail.com>
2020-11-05 16:15:18 +0300

    1

--- src/task9.kt
+++ src/task9.kt
@@ -1,11 +1,20 @@
 import kotlin.math.sqrt
 
 fun main() {
-    val a = readLine()?.toInt() ?: 0
-    val b = readLine()?.toInt() ?: 0
-    val c = readLine()?.toInt() ?: 0
-    //if (b <= c + a && c <= b + a && a <= b + c) {
+    var a = readLine()?.toInt() ?: 0
+    var b = readLine()?.toInt() ?: 0
+    var c = readLine()?.toInt() ?: 0
+    //if (b <= c + a && c <= b + a && a <= b + c)
+    while (b > a + c && c > a + b && a > c + b) {
+        a = readLine()!!.toInt()
+        b = readLine()!!.toInt()
+        c = readLine()!!.toInt()
+        println("кароч не правильно")
+    }
+
     if (b <= c + a && c <= b + a && a <= b + c) {
+        println("Ты молодец,наверняка матешу не прогуливал")
+
         val p = perimeter(a, b, c)
         val s = square(
             a.toDouble(), b.toDouble(),
@@ -13,9 +22,8 @@ fun main() {
         )
         println("Периметр равен $p")
         println("Площадь равна $s")
-    } else {
-        println("не существует")
     }
+
 }
 
 fun perimeter(a: Int, b: Int, c: Int): Int {
@@ -28,3 +36,7 @@ fun square(a: Double, b: Double, c: Double, p: Double): Double {
     val s = sqrt(hp * (hp - a) * (hp - b) * (hp - c))
     return s
 }
+//сделай так чтобы программа спрашивала данные пока пользователь не ведёт их правильно
+//if ( b > a + c && c > a + b && a > c + b) {
+//  println("неправильно пиши ещё раз")
+//}
