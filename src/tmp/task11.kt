59c9a8853b0c1d32f08efb9a1b0cd72307949a0a
ArcadiyAnatolevich <timatiun@gmail.com>
2020-11-16 19:02:30 +0300

    1

--- src/tmp/task11.kt
+++ src/tmp/task11.kt
@@ -10,8 +10,8 @@ fun main() {
     val quantityOfCargo = readLine()?.toInt() ?: 0
     val parallelepiped = cube(length, width, height)
     val volumeAndQuantity = volume * quantityOfCargo
-    val quantityOfBody = cal(parallelepiped, volumeAndQuantity)
-    val percentageOfWagonLoad = cir(volumeAndQuantity, parallelepiped)
+    val quantityOfBody = deductionOfCargo(parallelepiped, volumeAndQuantity)
+    val percentageOfWagonLoad = deductionOfCargoRercentage(volumeAndQuantity, parallelepiped)
     println("Кол-во вагонов равна $quantityOfBody")
     println("Процент загружености последнего вагона равен $percentageOfWagonLoad")
 }
@@ -20,12 +20,12 @@ fun cube(length: Int, width: Int, height: Int): Int {
     return length * width * height
 }
 
-fun cal(parallelepiped: Int, volumeAndQuantity: Int): Int {
+fun deductionOfCargo(parallelepiped: Int, volumeAndQuantity: Int): Int {
     val quantityOfBody = volumeAndQuantity / parallelepiped + if ((volumeAndQuantity % parallelepiped) > 0) 1 else 0
     return quantityOfBody
 }
 
-fun cir(volumeAndQuantity: Int, quantityOfCargo: Int): Int {
+fun deductionOfCargoRercentage(volumeAndQuantity: Int, quantityOfCargo: Int): Int {
     val percentageOfWagonLoad = volumeAndQuantity.toFloat() / quantityOfCargo.toFloat() * 100
     return percentageOfWagonLoad.roundToInt()
 }
